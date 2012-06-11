d48fc5aa5729e180f831cc2e20acdf9800c0a829
chenchunyi <ayi@creampecan.com>
2012-06-11 16:47:15 +0800

    Signin and Signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -9,13 +10,12 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
 
   def destroy
-    sign_out
-    redirect_to root_path
   end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,8 +4,14 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin" do
+  describe "signin page" do
+    before { visit signin_path }
 
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end
+
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -13,14 +19,9 @@ describe "Authentication" do
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-
-      describe "after visiting another page" do
-        before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
-      end
     end
 
-  describe "with valid information" do
+    describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",    with: user.email
@@ -32,11 +33,6 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
-      end
     end
   end
 end
